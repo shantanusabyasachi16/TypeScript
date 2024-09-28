b99396dcd33a311c83cd0e0504af6503218f0460
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-28 22:49:49 +0530

    functions in typescript

---
+++ Ts06/function.js
@@ -0,0 +1,15 @@
+function addTwo(num) {
+    return num + 2;
+}
+function getvalue(val) {
+    return val.toLowerCase;
+}
+function Signup(name, email, password) {
+}
+var loginuser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+addTwo(5);
+getvalue("DUGU");
+Signup("shantanu", "shantanu@gmail.com", true);
+loginuser("dugu", "dugu@gmail.com");
---
+++ Ts06/function.ts
@@ -0,0 +1,16 @@
+ function addTwo (num:number){
+    return num+2
+ }
+ function getvalue (val :string){
+    return val.toLowerCase
+ }
+
+ function Signup(name:string, email:string, password:boolean){
+
+ }
+ let loginuser =(name:string, email:string, isPaid:boolean =false) =>{}
+
+ addTwo(5)
+ getvalue("DUGU")
+ Signup("shantanu","shantanu@gmail.com",true)
+ loginuser ("dugu","dugu@gmail.com")
