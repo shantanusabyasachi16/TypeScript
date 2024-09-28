2989ae388ca95cf4e113e0eccdb30e6526a7557c
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-28 12:42:25 +0530

    defining types

---
+++ Ts05/variables.js
@@ -0,0 +1,3 @@
+// lets define a variable ony having string types
+var greetings = "hi shantanu";
+console.log(greetings);
---
+++ Ts05/variables.ts
@@ -0,0 +1,9 @@
+// lets define a variable ony having string types
+ let  greetings : string ="hi shantanu";
+
+ //greetings= true  //error
+ //greetings= 6  //error
+ //greetings.replace and it will only show thr methods associated with strings
+ console.log(greetings); // the variable is a string
+ 
+ export{}
