0eb4555717fca3f3a8b5071c5a9503c228428041
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-28 23:41:00 +0530

    functions in  typescript

--- Ts06/function.js
+++ Ts06/function.js
@@ -1,4 +1,22 @@
+"use strict";
+//  function addTwo (num:number){
+//     return num+2
+//  }
+//  function getvalue (val :string){
+//     return val.toLowerCase
+//  }
+Object.defineProperty(exports, "__esModule", { value: true });
+//  function Signup(name:string, email:string, password:boolean){
+//  }
+//  let loginuser =(name:string, email:string, isPaid:boolean =false) =>{}
+//  addTwo(5)
+//  getvalue("DUGU")
+//  Signup("shantanu","shantanu@gmail.com",true)
+//  loginuser ("dugu","dugu@gmail.com")
 function addTwo(num) {
+    //return num+2
+    //return "hello" // it will still work
+    //too avoid this we have to give a type to the function itself
     return num + 2;
 }
 function getvalue(val) {
@@ -9,7 +27,24 @@ function Signup(name, email, password) {
 var loginuser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
-addTwo(5);
+var userss = addTwo(5);
 getvalue("DUGU");
 Signup("shantanu", "shantanu@gmail.com", true);
 loginuser("dugu", "dugu@gmail.com");
+// function getuserss (myVal:number):string{
+// if (myVal>7) {
+//     return true
+// }
+// return '200 ok'
+// }
+//here we are getting more than one type
+//in arrow function
+var getdataa = function (name) {
+    return "";
+};
+//so here the typscript  automaticaly detets it is string or a number
+var cars = ["thar", "punch", "fortuner"];
+//const  cars =[1,2,3]
+cars.map(function (cars) {
+    return "car is ".concat(cars);
+});
--- Ts06/function.ts
+++ Ts06/function.ts
@@ -1,4 +1,25 @@
- function addTwo (num:number){
+//  function addTwo (num:number){
+//     return num+2
+//  }
+//  function getvalue (val :string){
+//     return val.toLowerCase
+//  }
+
+//  function Signup(name:string, email:string, password:boolean){
+
+//  }
+//  let loginuser =(name:string, email:string, isPaid:boolean =false) =>{}
+
+//  addTwo(5)
+//  getvalue("DUGU")
+//  Signup("shantanu","shantanu@gmail.com",true)
+//  loginuser ("dugu","dugu@gmail.com")
+
+
+function addTwo (num:number):number{
+    //return num+2
+    //return "hello" // it will still work
+    //too avoid this we have to give a type to the function itself
     return num+2
  }
  function getvalue (val :string){
@@ -10,7 +31,42 @@
  }
  let loginuser =(name:string, email:string, isPaid:boolean =false) =>{}
 
- addTwo(5)
+ let userss=addTwo(5)
  getvalue("DUGU")
  Signup("shantanu","shantanu@gmail.com",true)
  loginuser ("dugu","dugu@gmail.com")
+
+
+// function getuserss (myVal:number):string{
+// if (myVal>7) {
+//     return true
+// }
+// return '200 ok'
+// }
+//here we are getting more than one type
+//in arrow function
+const  getdataa = (name:string):string=>{
+return ""
+}
+
+
+//so here the typscript  automaticaly detets it is string or a number
+ const cars =["thar", "punch", "fortuner"]
+ //const  cars =[1,2,3]
+
+ cars.map( (cars):string=>{ // and for better syntax we should give the type
+return `car is ${cars}`
+ })
+
+
+ //never it means it never return a value
+// function error(errMsg:string):void{
+//     console.log(errMsg);
+    
+// }
+// instead of this we should user "never" for making errors more roboust 
+function error(errMsg:string):never{
+     // console.log(errMsg);
+        throw new Error(errMsg);
+ }
+ export {}
