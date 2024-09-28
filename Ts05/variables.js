b5e035e89a1ac5e1e79b16c9ae4cb5a141e3e476
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-28 13:02:53 +0530

    defining types

--- Ts05/variables.js
+++ Ts05/variables.js
@@ -1,3 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // lets define a variable ony having string types
 var greetings = "hi shantanu";
+greetings.toLowerCase();
+//greetings= true  //error
+//greetings= 6  //error
+//greetings.replace and it will only show thr methods associated with strings
 console.log(greetings);
+// number
+var userId = 334455.3;
+userId.toFixed();
+// userId = "shantanu"
+// boolean
+var isLoggedIn = false;
+// anyiables
+var hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
--- Ts05/variables.ts
+++ Ts05/variables.ts
@@ -1,9 +1,34 @@
 // lets define a variable ony having string types
- let  greetings : string ="hi shantanu";
+let greetings: string = "hi shantanu";
 
- //greetings= true  //error
+greetings.toLowerCase()
+//greetings= true  //error
  //greetings= 6  //error
  //greetings.replace and it will only show thr methods associated with strings
- console.log(greetings); // the variable is a string
- 
- export{}
+console.log(greetings);
+
+// number
+//let userId: number = 334455.3
+let userId = 334455.3// in number typescript can detect automatically it is number 
+
+userId.toFixed()
+// userId = "shantanu"// if we also don't define it will show error ... cannot no assign a string to it
+
+// boolean
+let isLoggedIn: boolean = false
+
+
+
+// anyiables
+
+let hero: string;
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+
+
+
+export {}
