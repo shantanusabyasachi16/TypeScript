ad37581f2551343b20c6db7a8eb0f4daebf1bf43
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-29 11:42:04 +0530

    type in ts

---
+++ Ts06/objects.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "shantanu",
+    email: "xyz@gmail.com",
+    isActive: true,
+};
+function createuser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+//object which return object
+createuser({ name: "shantanu", isPaid: true });
+function createCourse() {
+    return { name: "node.js", price: 2344 };
+}
+var Myuser = {
+    _id: "12345",
+    name: "shantanu",
+    email: "s.com",
+    isActive: true,
+    creaditcard: 123344
+};
+Myuser.email = "ss@gmail.com";
--- Ts06/objects.ts
+++ Ts06/objects.ts
@@ -15,4 +15,34 @@ function createCourse ():{name:string, price:number}{
 return{name:"node.js" , price:2344}
 }
 
+//type is kwyword in ts
+type User = {
+    _id:string,
+  name:string,
+  email: string,
+  isActive:boolean,
+   readonly creaditcard: number //cannot change or manipulate the value
+}
+let Myuser: User={
+    _id:"12345",
+    name:"shantanu",
+    email: "s.com",
+    isActive:true,
+    creaditcard:123344
+}
+Myuser.email = "ss@gmail.com"
+//Myuser.creaditcard=
+
+
+
+type CardNumber={
+    cardNumber:string
+}
+ type cardDate ={
+    cardDate: string
+ }
+
+ type CardDetails = CardNumber & cardDate  & {
+    cvv:number
+ }
 export{}
