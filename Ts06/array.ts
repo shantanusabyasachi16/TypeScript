677a9240bb71a9b1af501ca8e1bbe9cc00439ebe
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-10-01 09:22:26 +0530

    arrays in Ts

---
+++ Ts06/array.ts
@@ -0,0 +1,28 @@
+const marvel :string[]=[]
+
+const superpower :number[]=[]
+
+
+marvel.push("Ironman")
+
+superpower.push(6)
+
+//other way we can define
+const power : Array<number>=[]
+
+type User={
+    name:string,
+    isActive:boolean,
+}
+const allUsers: User[]=[]
+
+allUsers.push({name:"dugu" , isActive:true})
+
+// add an array in a array
+
+
+const models : number[][]=[
+    [255,255,255],
+    []
+]
+
