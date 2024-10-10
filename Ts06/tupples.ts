5518a08adce165680c623a3858e65dbcf25e42ba
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-10-10 12:10:46 +0530

    tupples in ts

---
+++ Ts06/tupples.ts
@@ -0,0 +1,15 @@
+/*A tuple is a typed array with a pre-defined length and types for each index.
+
+Tuples are great because they allow each element in the array to be a known type of value. */
+
+
+let tupples : [string, number,boolean]
+
+//tupples = [34,false,"thor"]
+tupples = ["thor",34,false]
+
+
+
+type userr = [number, string]
+
+ const newuserss :userr = [23,"dugu"]
