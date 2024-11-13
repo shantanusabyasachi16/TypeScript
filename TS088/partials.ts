929b88d32e52bdca30651234a724cffc4e027f36
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-11-13 11:56:49 +0530

    partial types in typescript

---
+++ TS088/partials.ts
@@ -0,0 +1,21 @@
+/**Partial changes all the properties in an object to be optional. */
+
+interface Todo{
+    id:number
+    completed:boolean
+}
+
+type upadatinput = Partial<Todo>;
+//The Partial<T> utility type in TypeScript takes an interface (or type) T and makes all of its properties optional. So Partial<Todo> will create a new type where both id and completed are optional.
+
+function updateTodo(id:number ,updates: upadatinput){
+
+}
+
+updateTodo(2,{
+
+completed:false 
+})
+
+
+//Since updates is of type Partial<Todo>, only" completed "is provided, and "id" is optional.
